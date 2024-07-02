985dec885738e21396cf4065a276e483fba76946
aladdin7127 <83582613+aladdin7127@users.noreply.github.com>
2024-07-02 17:26:35 +0100

    Update Renderer.cs

--- imgui-external-overlay-csharp/imgui-external-overlay-csharp/Renderer.cs
+++ imgui-external-overlay-csharp/imgui-external-overlay-csharp/Renderer.cs
@@ -1,16 +1,35 @@
-﻿using ClickableTransparentOverlay;
+using ClickableTransparentOverlay;
 using ImGuiNET;
+using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace imgui_render
 {
     public class Renderer: Overlay
     {
+        [DllImport("user32.dll")] static extern short GetAsyncKeyState(int vKey);
+
+        bool Show = true;
+        Vector2 WindowSize = new Vector2(300, 400);
+
         protected override void Render()
         {
-            ImGui.Begin("ImGui Overlay");
+            if ((GetAsyncKeyState(0x2D) & 1) > 0)
             {
-                ImGui.End();
+                Show = !Show;
             }
+
+            if (Show)
+            {
+                ImGui.SetNextWindowSize(WindowSize);
+                ImGui.Begin("ImGui Overlay", ImGuiWindowFlags.NoResize);
+                {
+                    ImGui.Checkbox("Show Menu", ref Show);
+
+                    ImGui.End();
+                }
+            }
+            
         }
     }
 }
